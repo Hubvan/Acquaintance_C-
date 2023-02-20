2fdbb869dca3ca8db7b053f0ae25c6d138a7226a
Ivan.P03dn <Po3dn.Ivan@gmail.com>
2023-02-20 19:42:41 +0300

    HW10

---
+++ HW10/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход 
+//трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трёхзначное число:");
+int.TryParse(Console.ReadLine(), out int chislo);
+
+string s = chislo.ToString();
+
+string result = s[1].ToString();
+
+Console.WriteLine($"Вторая цифра числа {s} : {result}");
