8d9071caac9c51491de1ccf2789786aad87170e3
Ivan.P03dn <Po3dn.Ivan@gmail.com>
2023-02-23 22:14:16 +0300

    Add commands HW2123

--- HW23/Program.cs
+++ HW23/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23. Напишите программу, которая принимает на 
+//вход число (N) и выдает таблицу кубов чисел от 1 до N.
+
+Console.WriteLine("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+int index = 1;
+
+while (index <= N)
+{
+  int result = (int)Math.Pow(index, 3);
+  Console.Write(result);
+  Console.Write(" ");
+  index++;
+}
