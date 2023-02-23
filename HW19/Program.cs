f7cd311c6474ba9403c8ec31d4f3ba5ac6fa3039
Ivan.P03dn <Po3dn.Ivan@gmail.com>
2023-02-23 22:43:19 +0300

    Add commands HW19

--- HW19/Program.cs
+++ HW19/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19. Напишите программу, которая принимает на вход 
+//пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите пятизначное число:");
+string chislo = Console.ReadLine();
+
+
+if (chislo[0] == chislo[chislo.Length-1] && chislo[1] == chislo[(chislo.Length-2)])
+//if (chislo[0] == chislo[4] && chislo[1] == chislo[3])
+{
+   Console.WriteLine($"Число {chislo} является палиндромом");
+}
+else
+{
+Console.WriteLine($"Число {chislo} не является палиндромом");
+}
