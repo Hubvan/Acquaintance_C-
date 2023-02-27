6e8124b04d19e224981ee6bcf8023172453c3de9
Hubvan <124492108+Hubvan@users.noreply.github.com>
2023-02-27 23:16:59 +0300

    Add commands HW25,27,29

--- HW25/Program.cs
+++ HW25/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите цикл, который принимает на вход два числа 
+//(А и В) и возводит число А в натуральную степень В.
+
+int Prompt (string message)
+{
+  Console.Write(message);
+  string readInput = System.Console.ReadLine();
+  int result = int.Parse(readInput);
+  return result;
+}
+
+int Power(int powerBase, int exponent)
+{
+  int power = 1;
+  for (int i = 0; i < exponent; i++)
+  {
+    power *= powerBase;
+  }
+
+  return power;
+}
+
+bool ValidateExponent(int exponent)
+{
+  if (exponent < 0)
+  {
+    Console.WriteLine("Показатель не должен быть меньше нуля");
+    return false;
+  }
+
+  return true;
+}
+
+int powerBase = Prompt("Введите основание: ");
+int exponent = Prompt("Введите показатель: ");
+if (ValidateExponent(exponent))
+{
+  Console.WriteLine($"Число {powerBase} в степени {exponent} равно {Power(powerBase, exponent)}");
+}
--- HW29/Program.cs
+++ HW29/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задает 
+//массив из 8 элементов, выводит их значение на экран.
+
+int Prompt(string message)
+{
+  Console.Write(message);
+  string ReadInput = Console.ReadLine();
+  int result = int.Parse(ReadInput);
+  return result;
+}
+
+int[] GenerateArray(int Length, int minValue, int maxValue)
+{
+  int[] array = new int[Length];
+  Random random = new Random();
+  for (int i = 0; i < Length; i++)
+  {
+    array[i] = random.Next(minValue, maxValue + 1);
+  }
+
+  return array;
+}
+
+void PrintArray(int[] array)
+{
+  Console.Write("|");
+  for (int i = 0; i < array.Length - 1; i++)
+  {
+    Console.Write($"{array[i]}, ");  
+  }
+  Console.Write($"{array[array.Length - 1]}");
+
+  Console.WriteLine("|");
+}
+
+int length = Prompt("Длина массива: ");
+int min = Prompt("Начальное значение, для диапозона случайного числа: ");
+int max = Prompt("Конечное значение, для диапозона случайного числа: ");
+int[] array = GenerateArray(length, min, max);
+
+PrintArray(array);
+
+
